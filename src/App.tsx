b1a30187801692974f5c0da71a69f42b47388a7f
PedroSampaioDev <pedrosampaio11@icloud.com>
2025-05-07 17:07:41 -0300

    fix: remove unuse

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { Footer } from "./Components/Footer";
 import { Main } from "./Components/Main";
-import { Navbar } from "./Components/Navbar";
 import { PageContainer } from "./Components/PageContainer";
 export const App: React.FC = () => {
   useEffect(() => {
@@ -12,9 +10,7 @@ export const App: React.FC = () => {
 
   return (
     <PageContainer>
-      <Navbar/>
       <Main />
-      <Footer />
     </PageContainer>
   );
 };
--- src/Components/Main/index.tsx
+++ src/Components/Main/index.tsx
@@ -1,21 +1,9 @@
 import { MainContainer } from "./styles";
-import { UseState } from "../Hooks/UseState";
-import { Lista } from "../Listas/Lista";
-import { StateLift } from "../States/StateLift";
-import { Cond } from "../Condicional";
-import { Props } from "../Props";
-import { FormSection } from "../Forms/FormSection";
-export interface PropsType {
-  name: string;
-}
-const name = "pedro"
+
 export const Main: React.FC = () => {
-  return <MainContainer  >
-    <UseState />
-    <Lista/>
-    <Cond/>
-    <Props name= {name}/>
-    <StateLift/>
-    <FormSection/>
-  </MainContainer>;
+  return (
+    <MainContainer>
+      <h1>Bem-vindo</h1>
+    </MainContainer>
+  );
 };
