9645e1cb07c706c742259cbf20a43f6173fbbbec
PedroSampaioDev <pedrosampaio11@icloud.com>
2025-05-08 19:16:55 -0300

    feat: config json server

--- src/Components/Main/index.tsx
+++ src/Components/Main/index.tsx
@@ -3,7 +3,7 @@ import { MainContainer } from "./styles";
 export const Main: React.FC = () => {
   return (
     <MainContainer>
-      <h1>Bem-vindo</h1>
+      <h1>Listas De Produtos</h1>
     </MainContainer>
   );
 };
