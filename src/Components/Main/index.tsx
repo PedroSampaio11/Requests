c995166d28ca948e0f92aeee336b37fafc716578
PedroSampaioDev <pedrosampaio11@icloud.com>
2025-05-08 21:27:03 -0300

    feat: add new component

--- src/Components/Main/index.tsx
+++ src/Components/Main/index.tsx
@@ -1,9 +1,11 @@
 import { MainContainer } from "./styles";
-
+import { ResgatandoDados } from "../Resgate";
 export const Main: React.FC = () => {
+
   return (
     <MainContainer>
       <h1>Listas De Produtos</h1>
+      <ResgatandoDados/>
     </MainContainer>
   );
 };
---
+++ src/Components/Resgate/index.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+interface Product {
+  id: string; // id é uma string
+  name: string; // name é uma string
+  price: number; // price é um número
+}
+export const ResgatandoDados: React.FC = () => {
+  // URL de onde queremos pegar os dados
+  const url = "http://localhost:3000/products";
+
+  // Cria uma "variável" chamada 'products' que começa vazia
+  const [products, setProducts] = useState<Product[]>([]); 
+
+  // Esse código roda quando o componente for carregado
+  useEffect(() => {
+    // Função que pega os dados da API
+    const fetchData = async () => {
+      const res = await fetch(url); // Faz a requisição
+      const data = await res.json(); // Converte a resposta para JSON
+
+      // Atualiza a variável 'products' com os dados que pegamos
+      setProducts(data);
+    };
+
+    fetchData(); // Chama a função para pegar os dados
+  }, []); // O efeito só roda uma vez, quando o componente for carregado
+
+  console.log(products); // Mostra os dados no console para você ver
+
+  return (
+    <>
+      <h2>Resgatando Dados com Fetch API</h2>
+      <ul>
+        {products.map(
+          (p) => (
+            <li key={p.id}>
+        {p.name}
+            </li>
+          )
+        )}
+      </ul>
+    </>
+  );
+};
